f3ebb5974c1747e4dbb7865f54095b4e4093b96e
Deepakpd811 <deepak0413.be21@chitkara.edu.in>
2025-04-17 11:58:19 +0530

    UC3 show name using pathvariable at /parem/{anyname}

--- src/main/java/com/restapi/demo/controller/HelloController.java
+++ src/main/java/com/restapi/demo/controller/HelloController.java
@@ -9,15 +9,24 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/")
 public class HelloController {
 
+    //uc1
     @RequestMapping(value = {"","/","/home"} )
     public String sayHello(Model model) {
         return "hello  from Bridgelab";
     }
 
+    //uc2
     @RequestMapping(value = {"/query"})
     public String sayHello(@RequestParam(value = "name") String name){
         return "Hello " + name;
     }
+
+    // uc3
+    @RequestMapping(value = {"/parm/{name}"})
+    public String sayHellobypath(@PathVariable String name){
+        return "this msg from url Hello " + name;
+    }
+
 
 
 
