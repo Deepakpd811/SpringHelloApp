263477432d643f9c4bbba61d23f40697e259b036
Deepakpd811 <deepak0413.be21@chitkara.edu.in>
2025-04-17 11:45:50 +0530

    UC1 create rest controller call hello bridgelab usring get route /

---
+++ src/main/java/com/restapi/demo/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.restapi.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+    @RequestMapping(value = {"","/","/home"} )
+    public String sayHello(Model model) {
+        return "hello  from Bridgelab";
+    }
+
+
+}
