046a2109da5d98369189ce761867a0815fe6a80b
Deepakpd811 <deepak0413.be21@chitkara.edu.in>
2025-04-17 13:46:55 +0530

    [Deepak]add: UC5 set firstname and lastname by usnig request param and pathvariable route is /put/deepak?lastname=prasad

--- src/main/java/com/restapi/demo/controller/HelloController.java
+++ src/main/java/com/restapi/demo/controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.restapi.demo.controller;
 
 import com.restapi.demo.dto.UserDto;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,29 +10,38 @@ import org.springframework.web.bind.annotation.*;
 public class HelloController {
 
     //uc1
-    @RequestMapping(value = {"","/","/home"} )
+    @RequestMapping(value = {"", "/", "/home"})
     public String sayHello(Model model) {
         return "hello  from Bridgelab";
     }
 
     //uc2
     @RequestMapping(value = {"/query"})
-    public String sayHello(@RequestParam(value = "name") String name){
+    public String sayHello(@RequestParam(value = "name") String name) {
         return "Hello " + name;
     }
 
     // uc3
-    @RequestMapping(value = {"/parm/{name}"} , method = RequestMethod.GET)
-    public String sayHellobypath(@PathVariable String name){
+    @RequestMapping(value = {"/parm/{name}"}, method = RequestMethod.GET)
+    public String sayHellobypath(@PathVariable String name) {
         return "this msg from url Hello " + name;
     }
 
     // uc4
     @PostMapping("/post")
-    public String sayhelloDto(@RequestBody UserDto user){
-        return "Hello " + user.getFirstname()+" "+user.getLastname();
+    public String sayhelloDto(@RequestBody UserDto user) {
+        return "Hello " + user.getFirstname() + " " + user.getLastname();
     }
 
+    //uc5
+    //http://localhost:8080/put/deepak?lastname=prasad
+    @PutMapping("/put/{firstname}")
+    public String helloPUT(
+            @PathVariable String firstname,
+            @RequestParam String lastname
+    ) {
+        return "Hello " + firstname + " " + lastname;
+    }
 
 
 }
